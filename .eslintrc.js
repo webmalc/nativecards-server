58d1cff4e3e59babc16372a7793675c6f9427817
webmalc <m@webmalc.pw>
2018-08-17 23:55:05 +0300

    the initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true,
+    },
+    "extends": "google",
+    "rules": {
+        "no-var": 0,
+        "no-invalid-this": 0
+ }}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Nativecards
+The backend server for Nativecards app
+
+## urls
+* two-factor setup url: account/two_factor/setup/
