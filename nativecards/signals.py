30eaaead175a8394e7e87db507737aa970c00e61
webmalc <m@webmalc.pw>
2018-09-25 17:44:59 +0300

    add removing the cache on delete

--- nativecards/signals.py
+++ nativecards/signals.py
@@ -1,8 +1,13 @@
 from django.core.cache import cache
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from .models import CachedModel
+
+
+def _clear_cache(instance):
+    if isinstance(instance, CachedModel):
+        cache.clear()
 
 
 @receiver(post_save, dispatch_uid='cached_model_post_save')
@@ -10,5 +15,12 @@ def cached_model_post_save(sender, **kwargs):
     """
     Cached model post save
     """
-    if isinstance(kwargs['instance'], CachedModel):
-        cache.clear()
+    _clear_cache(kwargs['instance'])
+
+
+@receiver(pre_delete, dispatch_uid='cached_model_pre_delete')
+def cached_model_pre_delete(sender, **kwargs):
+    """
+    Cached model pre delete
+    """
+    _clear_cache(kwargs['instance'])
