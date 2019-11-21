fb4ff4ac1c350975170547bf7c710fd7f8e26f7c
webmalc <m@webmalc.pw>
2019-11-21 16:12:38 +0300

    Move the project settings to the django-environ module

--- nativecards/test/test_messenger.py
+++ nativecards/test/test_messenger.py
@@ -1,23 +1,26 @@
+import pytest
 from django.contrib.auth.models import User
 
 from nativecards.lib.messengers import mailer
 
+pytestmark = pytest.mark.django_db
 
-def test_mail_managers(admin_client, mailoutbox):
+
+def test_mail_managers(mailoutbox):
     mailer.mail_managers(subject='Text message', data={'text': 'Test text'})
     assert len(mailoutbox) == 1
     mail = mailoutbox[0]
+
     assert mail.recipients() == ['admin@example.com', 'manager@example.com']
     assert 'Text message' in mail.subject
     assert 'Test text' in mail.alternatives[0][0]
 
 
-def test_mail_client_by_email(admin_client, mailoutbox):
-    mailer.mail_user(
-        subject='Test message',
-        template='emails/registration.html',
-        data={},
-        email='client@example.com')
+def test_mail_client_by_email(mailoutbox):
+    mailer.mail_user(subject='Test message',
+                     template='emails/registration.html',
+                     data={},
+                     email='client@example.com')
 
     assert len(mailoutbox) == 1
     mail = mailoutbox[0]
@@ -26,14 +29,13 @@ def test_mail_client_by_email(admin_client, mailoutbox):
     assert 'Your registration was successful' in mail.alternatives[0][0]
 
 
-def test_mail_client_by_client(admin_client, mailoutbox):
+def test_mail_client_by_client(mailoutbox):
     def send(user):
         user = User.objects.get(username=user)
-        mailer.mail_user(
-            subject='Test message',
-            template='emails/registration.html',
-            data={},
-            user=user)
+        mailer.mail_user(subject='Test message',
+                         template='emails/registration.html',
+                         data={},
+                         user=user)
 
     send('admin')
     assert len(mailoutbox) == 1
