bd846adf83e064759924ad24f049bc3977f79cac
webmalc <m@webmalc.pw>
2022-12-05 17:02:34 +0300

    Add the docker

--- users/mailer.py
+++ users/mailer.py
@@ -2,6 +2,7 @@
 The users mailer module
 """
 from django.contrib.auth.models import User
+
 from nativecards.lib.messengers import mailer
 
 
@@ -9,10 +10,12 @@ def send_registration_email(user: User) -> None:
     """
     Send the registration to the user
     """
-    mailer.mail_user(subject='Welcome to Nativecards',
-                     template='emails/registration.html',
-                     data={
-                         'url': user.profile.verification_code,
-                         'username': user.username
-                     },
-                     user=user)
+    mailer.mail_user(
+        subject='Welcome to Nativecards',
+        template='emails/registration.html',
+        data={
+            'url': user.profile.verification_code,
+            'username': user.username
+        },
+        user=user,
+    )
